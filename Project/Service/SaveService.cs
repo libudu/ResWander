5c0c2d46c4c423aeb5cbb7330b61c8a8d3c1fc99
riceblast <15198795025@163.com>
2020-05-28 23:38:49 +0800

    图片保存，测试用例

--- Project/Service/SaveService.cs
+++ Project/Service/SaveService.cs
@@ -1,15 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace ResWander.Service
 {
     /// <summary>
     /// 负责对不同类型资源进行本地保存的父类
+    /// 对前端封装图片保存的方法
     /// </summary>
     public class SaveService
     {
+        /// <summary>
+        /// 前端调用接口：图片文件存储
+        /// </summary>
+        /// <param name="imageSources">待保存的图片文件</param>
+        public static void SaveImages(List<Image> imageSources)
+        {
+            ImageSaver.SaveImage(imageSources);
+        }
+    }
+
+    /// <summary>
+    ///  用于提供和保存图片相关函数的类
+    /// </summary>
+    internal class ImageSaver
+    {
+        /// <summary>
+        /// 用于保存指定的图片资源
+        /// </summary>
+        /// <param name="imageSources">待保存的图片资源</param>
+        internal static void SaveImage(List<Image> imageSources)
+        {
+            // 利用C#封装好的类进行图片等资源的保存
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            // 设置保存的格式等相关信息
+            saveFileDialog.Filter = "Jpg 图片|*.jpg|Bmp 图片|*.bmp|Gif 图片|*.gif|Png 图片|*.png|Wmf  图片|*.wmf";
+            saveFileDialog.FilterIndex = 0;
+            saveFileDialog.RestoreDirectory = true; //保存对话框记忆上次打开的目录
+
+            // 打开对话框并保存
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // 依次保存每个文件
+                for(int i = 0; i < imageSources.Count; i++)
+                {
+                    imageSources[i].Save(saveFileDialog.FileName +" " + i);
+
+                }
+
+                // 提示用户保存成功
+                MessageBox.Show("已成功保存图片", "保存成功");
+            }
+        }
     }
 }
---
+++ ResWanderUnitTest/ServiceTest/SaveServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResWanderUnitTest.ServiceTest
+{
+    /// <summary>
+    /// 保存服务测试类
+    /// </summary>
+    class SaveServiceTest
+    {
+    }
+}
