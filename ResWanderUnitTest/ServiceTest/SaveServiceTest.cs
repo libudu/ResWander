d7669cf2b4a13d08012d8046850adde0e56061c3
riceblast <15198795025@163.com>
2020-05-29 10:02:29 +0800

    添加保存文件单元测试

--- ResWanderUnitTest/ServiceTest/SaveServiceTest.cs
+++ ResWanderUnitTest/ServiceTest/SaveServiceTest.cs
@@ -3,13 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ResWander.Service;
 
 namespace ResWanderUnitTest.ServiceTest
 {
     /// <summary>
-    /// 保存服务测试类
+    /// 保存服务测试类(SaveService 测试类)
     /// </summary>
-    class SaveServiceTest
+    [TestClass]
+    public class SaveServiceTest
     {
+        private List<Image> imageSources;
+
+        /// <summary>
+        /// 在方法调用之前初始化资源文件
+        /// </summary>
+        [ClassInitialize]
+        public void InitSourceFiles()
+        {
+            imageSources = new List<Image>();
+            imageSources.Add(new Bitmap("/测试文件/图片.png"));
+        }
+
+        /// <summary>
+        /// 存储照片方法初始化
+        /// </summary>
+        [TestMethod]
+        public void ImageSaveTest()
+        {
+            SaveService.SaveImages(this.imageSources);
+        }
     }
 }
