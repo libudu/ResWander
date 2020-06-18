cd1a22863960489e0ae6cda97e3e7ebd3044c1e6
LXP <1944256523@qq.com>
2020-06-18 11:55:34 +0800

    修复冲突

--- Project/Service/SaveService.cs
+++ Project/Service/SaveService.cs
@@ -21,9 +21,9 @@ namespace ResWander.Service
         /// 前端调用接口：图片文件存储
         /// </summary>
         /// <param name="imageSources">待保存的图片文件</param>
-        public static void SaveImages(List<ImgResource> imageSources)
+        public static string SaveImages(List<ImgResource> imageSources)
         { 
-            ImageSaver.SaveImage(imageSources);
+            return ImageSaver.SaveImage(imageSources);
         }
 
         /// <summary>
@@ -37,9 +37,9 @@ namespace ResWander.Service
         /// <summary>
         /// 
         /// </summary>
-        public static void SaveKeywordImages(string keyword, List<ImgResource> imageSources)
+        public static string SaveKeywordImages(string keyword, List<ImgResource> imageSources)
         {
-            KeywordImgSaver.SaveKeywordImg(keyword, imageSources);
+            return KeywordImgSaver.SaveKeywordImg(keyword, imageSources);
         }
     }
 
@@ -52,8 +52,10 @@ namespace ResWander.Service
         /// 用于保存指定的图片资源
         /// </summary>
         /// <param name="imageSources">待保存的图片资源</param>
-        internal static void SaveImage(List<ImgResource> imageSources)
+        internal static string SaveImage(List<ImgResource> imageSources)
         {
+            string localFilePath;
+            string filePath;
             // 利用C#封装好的类进行图片等资源的保存
             SaveFileDialog saveFileDialog = new SaveFileDialog();
 
@@ -65,17 +67,22 @@ namespace ResWander.Service
             // 打开对话框并保存
             if (saveFileDialog.ShowDialog() == DialogResult.OK)
             {
+                //获得文件路径 
+                localFilePath = saveFileDialog.FileName.ToString();
+                //获取文件路径，不带文件名 
+                filePath = localFilePath.Substring(0, localFilePath.LastIndexOf("\\"));
+
                 // 依次保存每个文件
-                for(int i = 0; i < imageSources.Count; i++)
+                for (int i = 0; i < imageSources.Count; i++)
                 {
                     string[] fileName = saveFileDialog.FileName.Split('.');
                     //若选择自适应，按照图片格式保存图片
-                    if(saveFileDialog.FilterIndex == 1)
+                    if (saveFileDialog.FilterIndex == 1)
                     {
                         string format = string.Empty;
                         ImageService.GetImageFormat(imageSources[i].Img, out format);
 
-                        if(format == string.Empty)
+                        if (format == string.Empty)
                         {
                             format = "jpg";
                         }
@@ -86,10 +93,11 @@ namespace ResWander.Service
                         imageSources[i].Img.Save(fileName[0] + " " + i + "." + fileName[1]);
                     }
                 }
-
                 // 提示用户保存成功
                 MessageBox.Show("已成功保存图片", "保存成功");
+                return filePath;
             }
+            return null;
         }
     }
 
@@ -116,12 +124,13 @@ namespace ResWander.Service
             Save(savePath, keyword, imageSources);
         }
 
-        internal static void SaveKeywordImg(string keyword, List<ImgResource> imageSources)
+        internal static string SaveKeywordImg(string keyword, List<ImgResource> imageSources)
         {
             string savePath = SelectPath();
 
             //将图片保存在路径中
             Save(savePath, keyword, imageSources);
+            return savePath;
         }
 
         private static string SelectPath()
